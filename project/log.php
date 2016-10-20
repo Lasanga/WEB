95f62c64fa6c784854e1db6fbd1616282f9f14a9
osura1 <osuramahanama11@gmail.com>
2016-10-20 22:07:33 +0530

    Add files via upload

--- project/log.php
+++ project/log.php
@@ -1,17 +1,43 @@
 <?php
-include 'core/functions/functions.php';
+
 $link=mysqli_connect('localhost','root','','suser');
 
-
-global $user;
 $user = $_POST['un'];
 $pass = $_POST['pw'];
-$pass =md5($pass);
 
 
-login($user,$pass);
+
+
+
+
+$sql=mysqli_query($link,"select * from sign where username='$user' and password='$pass");
+echo "$sql";
+die();
+$row = mysqli_fetch_array($sql);
+
+if($row['username'] == $user && ['password'] == $pass )
+{
+	echo "login successful";
+}
+else{
+	echo "login failed";
+}
+
 
 
 
 
 ?>
+<!doctype html>
+<html>
+<head>
+<title>LMS-Learning Management System</title> 
+<meta charset="UTF-8">
+<link rel="icon" href="main.ico" type="type/x-icon">
+</head>
+<body>
+
+<h1>You are signed in</h1>
+
+</body>
+</html>
--- project/sign.php
+++ project/sign.php
@@ -13,3 +13,16 @@ $sql="insert into sign(username,year,email,password)values('$user','$yer','$emai
 $rel=mysqli_query($link,$sql);
 
 ?>
+<!doctype html>
+<html>
+<head>
+<title>LMS-Learning Management System</title> 
+<meta charset="UTF-8">
+<link rel="icon" href="main.ico" type="type/x-icon">
+</head>
+<body>
+
+<h1>You are signed in</h1>
+
+</body>
+</html>
