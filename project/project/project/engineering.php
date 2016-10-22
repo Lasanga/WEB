72309c12467834e12cafe2a7f80e8bec5adabaff
rumeshhh <asirirumeshfernando@gmail.com>
2016-10-21 22:01:09 -0700

    Add files via upload

---
+++ project/project/project/engineering.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+href="managementc.css">
+</head>
+<body background="img4.jpg">
+<h1>Faculty Of Engineering</h1>
+<p>The Faculty of Engineering offers degrees in:</p>
+<ul>
+<li>Civil Engineering</li>
+<li>Materials Engineering</li>
+<li>Mechanical Engineering</li>
+<li>Electronics</li>
+</ul>
+<p>in collaboration with the internationally acclaimed universities. Faculty of Engineering prides itself for keeping up to date with the advancing technologies through research and development activities, staff training and networking with international communities. Further, the Faculty hopes to acquire, promote, develop and disseminate knowledge of engineering sciences in order to improve the quality of life by equipping future generations with the right attitudes and skills to grow into competent individuals who could be considered an asset to the nation. The primary aim of the Faculty is forming engineers of the highest quality who, with experience, should be able to hold responsible positions at the highest levels of the profession, possessing the wisdom to recognize their professional development.
+</p>
+<h2>Programmes:</h2>
+<h3>
+<ul id="x"><pre>
+<li>BSc Engineering (Hons) Civil Engineering</li>
+<li>BSc Engineering (Hons) Electrical & Electronic Engineering</li>
+<li>BSc Engineering (Hons) Materials Engineering</li>
+<li>BSc Engineering (Hons) Mechanical Engineering</li>
+<li>BSc Engineering (Hons) Mechanical Engineering</li>
+</pre></ul>
+</h3>
+
+</body>
+</html>
---
+++ project/project/project/management.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+href="managementc.css"/>
+</head>
+<body background="img2.jpg">
+<h1>Faculty Of Management</h1>
+<p>The Faculty of Business is the ideal institute for any undergraduate interested in 
+pursuing a career in the Business field. The Faculty nurtures students with a business mind 
+and moulds them into fully fledged business leaders of the future. The drive behind achieving this
+ goal is the passion for excellence and perfection that surrounds the School of Business in its methods 
+of teaching, learning, research and networking with the business community. The Faculty of Business offers
+ many degree programs:</p> <ul><li>Business Management</li>
+			<li>Human Resource Management</li>
+			<li>Law</li>
+			<li>Accounting</li>
+	</ul><p>This gives our graduates the opportunity to gain an International degree while living
+ and working in Sri Lanka. Students will find learning at Faculty of Business quite a unique and 
+interesting experience as undergraduates are also given the taste of real life business experiences 
+while learning the theories behind it at class. Through this process, the Faculty strives to prepare
+ business undergraduates to face any challenges in the real business world as they will be equipped 
+with excellent problem solving and analytical capabilities. The Faculty of Business takes special care
+ to ensure that all students are provided with intellectual depth and abundant resources as well as 
+individual attention.Each department of the Faculty of Business is dedicated to its students and in 
+providing them the best possible educational experience. So, if you’re looking for a bright future
+ in Business, Faculty of Business is the place to be!
+</p>
+<h2>Programmes:</h2>
+<h3>
+<ul id="x">
+<pre>
+<li>Postgraduate Diploma in Human Resource Management</li>
+<li>Postgraduate Diploma in Business Management</li>
+<li>BSc in Business Management (Human Resource Management) (Special)</li>
+<li>BSc in Business Management (Logistics Management) (Special)</li>
+<li>BSc in Business Management (industrial management) (Special)</li>
+<li>BSc in Business Management (Project management) (Special)</li>
+<li>BA in Business Communication</li>
+</pre>
+</ul>
+</h3>
+
+</body>
+</html>
