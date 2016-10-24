b2317aa322feb98e6d6add34fb18be6043749627
osura1 <osuramahanama11@gmail.com>
2016-10-25 00:36:34 +0530

    Add files via upload

---
+++ osura/about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="learning management system">
+<meta name="keywords" content="html,css,javascript,php">
+<meta name="author" content="osura mahanama">
+
+<link rel="icon" href="images/main.ico" type="image/x-icon">
+<link href="css/about.css" rel="stylesheet" type="text/css">
+<link href = "css/home.css" rel = "stylesheet" type="text/css">
+
+
+<title>LMS-Learning Management System</title>
+
+</head>
+
+<body>
+
+<div id="head">
+
+<h1 align="center"><u><b> ABOUT US</b> </u></h1>
+
+</div>
+
+<div class="mid">
+
+<p id="desc">CYNTEC offers undergraduate and postgraduate degree programs in Management,Computing and Engineering.Innovation is the key at CYNTEC as we understand the <br>importance of originality and creativity in this world.
+The education system in CYNTECH is therefore designed with intension of ensuring that all our graduates are fully prepared to face any challenges the world has to offer.
+CYNTEC offers world renowned degrees that are much more than just a certificate.Our lecture panel skillfully integrates leadership,ethics,global thinking,core management skills and technological innovations into learning process to make
+our graduates well rounded global citizens.Our undergraduates enjoy a healthy work-life balance, as CYNTEC encourages students to develop and pursue their talents and hobbies through various ecents and activities like
+their the Annual Sports Day and the celebrations of multicultural festivals which showcasetheir creative and musical abilitis. And here in CYNTEC we have useful clubs like the Rotaract Club, Leo Club and Red Cross society.
+Student take participation in these clubs and they can work for the betterment of community.We provide <br> many indoor activities and outdoor activities. We have different kinds of sports and activities.</P> 
+
+
+<div class="slide">
+
+
+<img id="img" src="images/6.jpg" alt="image1" >
+
+<hr>
+<div>
+<p> CYNTEC is working at its full capacity to integrate innovation, entrepreneurship, with a culturally and ethically diverse workforce and international engagement of<br>
+expertise in the fields of Information Technology and Management. It will assist to build integrating leadership, ethics,
+global thinking, core management skills, and leading edge technological innovations. <br> As a forward thinking school in higher education CYNTEC assures the vital need to make learning relavant to industry expectations.
+CYNTEC has forges strategic collaborations with leading universities in the world that enables its students to expose themselves to global thinking and best practices in global industries.</P>
+
+
+
+ <p><u> Our Facilities : </u></p>
+ <ul>
+ <li>We have our own computer labs. </li>
+ <li>We have the best lecturers from around the world. </li>
+ <li>We provide an acommodation system for our students.</li>
+ <li>We provide a shuttle service throughout the country. </li>
+ <li>We have our own gym,swimming pools and a ground.</li>
+ </ul>
+
+
+
+</div>
+
+</body>
+</html>
